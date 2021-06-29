97f4bb65fd5b2e0cd0222084ea92367a2b61c548
Sebastian Weigand <s.weigand.phy@gmail.com>
2021-06-29 17:48:42 +0200

    🧪Add proper tests (#123)
    * 🔧 Changed editor chonfig to work with python
    
    * 🧪 Added integration tests
    
    * 🩹 Made syscalls py2 and py3 compatible
    
    * 🧪 Added tests for env vars
    
    Will be activated new activation in the next PR for the new activation method
    
    * 🧹 Cleand up workflow console output
    
    * 🧪 Added tests for installed binaries
    
    * 🧪 Added tests for env python and conda python
    
    * 🧪 Added test for channels
    
    * 🧹 Named jest-test workflow job

---
+++ integrationtests/prepare_tests.py
@@ -0,0 +1,34 @@
+import json
+import subprocess
+import sys
+
+
+def write_initial_settings():
+    """Write initial settings to a file"""
+    initial_settings = {
+        "executable": sys.executable,
+        "version_info": str(sys.version_info),
+    }
+    with open("initial_settings.json", "w") as f:
+        json.dump(initial_settings, f)
+    print(json.dumps(initial_settings, indent=2))
+
+
+def read_initial_settings():
+    """Read initial settings to a file"""
+    with open("initial_settings.json") as f:
+        return json.load(f)
+
+
+def run_cmd(cmd):
+    """Run shell commend in a python2/3 compatible way"""
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    process.wait()
+    stdout, stderr = process.communicate()
+    return process.returncode, stdout, stderr
+
+
+if __name__ == "__main__":
+    write_initial_settings()
