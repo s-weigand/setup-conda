2699d8417d89d14f2bf9660e0e2d47c1d2c59b16
s-weigand <s.weigand.phy@gmail.com>
2021-06-29 17:33:14 +0200

    🧪 Added test for channels

--- integrationtests/test_common.py
+++ integrationtests/test_common.py
@@ -11,6 +11,19 @@ def test_conda_installed():
     assert stderr == b""
 
 
+def test_conda_channels():
+    """Conda channels are added in order"""
+    returncode, stdout, stderr = run_cmd(" conda config --show channels")
+    expected = os.environ["CONDA_CHANNELS"].split(",")
+    channel_list = stdout.decode().partition(":")[2].split(" - ")
+    channel_list = [channel.strip() for channel in channel_list]
+    channel_list.remove("")
+
+    assert returncode == 0
+    assert channel_list == expected
+    assert stderr == b""
+
+
 # def test_conda_env_vars_set():
 #     """Conda env_vars are set"""
 #     assert "miniconda" in os.environ["CONDA_PREFIX"].lower()
