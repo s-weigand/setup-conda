6beb34d0bdeed218ba49b9c84636185750cb643f
Sebastian Weigand <s.weigand.phy@gmail.com>
2024-05-17 16:45:53 +0200

    ⬆️🧰 Update action run definition, dependencies and .pre-commit-config.yaml (#436)
    This updates the action run definition from node 16 to node 20 to get rid of the deprecation warning.
    And updates all dependencies and the .pre-commit-config.yaml.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- integrationtests/test_python_version.py
+++ integrationtests/test_python_version.py
@@ -8,7 +8,7 @@ from integrationtests.prepare_tests import read_initial_settings
 
 @pytest.mark.skipif("NOT_ACTIVATED" not in os.environ, reason="Conda python is used")
 def test_python_not_changed():
-    """Same python as initially"""
+    """Same python as initially."""
     initial_settings = read_initial_settings()
     assert initial_settings["executable"] == sys.executable
     assert initial_settings["version_info"] == str(sys.version_info)
@@ -16,7 +16,7 @@ def test_python_not_changed():
 
 @pytest.mark.skipif("NOT_ACTIVATED" in os.environ, reason="Conda python is not used")
 def test_python_from_conda():
-    """Same python if from conda"""
+    """Same python if from conda."""
     initial_settings = read_initial_settings()
     assert initial_settings["executable"] != sys.executable
     assert "miniconda" in sys.executable.lower()
@@ -24,11 +24,11 @@ def test_python_from_conda():
 
 @pytest.mark.skipif("ENV_PYTHON" not in os.environ, reason="Not a conda custom python")
 def test_custom_python_from_conda():
-    """Same python if from conda"""
+    """Same python if from conda."""
     assert sys.version.startswith(os.environ["ENV_PYTHON"])
 
 
 @pytest.mark.skipif("PYPY_TEST" not in os.environ, reason="Not a pypy test")
 def test_pypy_from_conda():
-    """Installed python version is PyPy"""
+    """Installed python version is PyPy."""
     assert sys.version.splitlines()[1].startswith("[PyPy")
