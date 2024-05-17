4dd1842673264b2a20c08fb3e10707fe9f1bd4bd
s-weigand <s.weigand.phy@gmail.com>
2024-05-17 15:46:14 +0200

    🧹 Run pre-commit on all files

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    env: {
+      es6: true,
+      node: true,
+    },
+    rules: {
+      'no-var': 'error',
+      semi: ['error', 'never'],
+      indent: ['error', 2],
+      'no-multi-spaces': 'error',
+      'space-in-parens': 'error',
+      'no-multiple-empty-lines': 'error',
+      'prefer-const': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+]
--- integrationtests/prepare_tests.py
+++ integrationtests/prepare_tests.py
@@ -4,7 +4,7 @@ import sys
 
 
 def write_initial_settings():
-    """Write initial settings to a file"""
+    """Write initial settings to a file."""
     initial_settings = {
         "executable": sys.executable,
         "version_info": str(sys.version_info),
@@ -15,13 +15,13 @@ def write_initial_settings():
 
 
 def read_initial_settings():
-    """Read initial settings to a file"""
+    """Read initial settings to a file."""
     with open("initial_settings.json") as f:
         return json.load(f)
 
 
 def run_cmd(cmd):
-    """Run shell command in a python2/3 compatible way"""
+    """Run shell command in a python2/3 compatible way."""
     process = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
     )
