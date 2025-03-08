41c769c810cc0ae68cdf7ba3b6371e45c64e97af
s-weigand <s.weigand.phy@gmail.com>
2025-03-09 00:00:15 +0100

    🧪⬆️ Update unite test data for activation scripts
    No update to the mac scripts since there are no mac runners with preinstalled conda, but it should pretty much be the same as the linux ones

--- __tests__/data/windows_conda_powershell_activation.ps1
+++ __tests__/data/windows_conda_powershell_activation.ps1
@@ -1,9 +1,9 @@
-$Env:PATH = "C:\Miniconda\envs\__setup_conda;C:\Miniconda\envs\__setup_conda\Library\mingw-w64\bin;C:\Miniconda\envs\__setup_conda\Library\usr\bin;C:\Miniconda\envs\__setup_conda\Library\bin;C:\Miniconda\envs\__setup_conda\Scripts;C:\Miniconda\envs\__setup_conda\bin;C:\Miniconda\condabin;C:\Miniconda\Scripts;C:\Miniconda;C:\Program Files\MongoDB\Server\5.0\bin"
+$Env:_CE_M = $null
+$Env:_CE_CONDA = $null
+$Env:PATH = "C:\Miniconda\envs\__setup_conda;C:\Miniconda\envs\__setup_conda\Library\mingw-w64\bin;C:\Miniconda\envs\__setup_conda\Library\usr\bin;C:\Miniconda\envs\__setup_conda\Library\bin;C:\Miniconda\envs\__setup_conda\Scripts;C:\Miniconda\envs\__setup_conda\bin;C:\Miniconda\condabin;C:\Miniconda\Scripts;C:\Miniconda\Library\bin;C:\Miniconda;C:\Program Files\MongoDB\Server\5.0\bin;C:\aliyun-cli;C:\vcpkg;C:\Program Files (x86)\NSIS;C:\tools\zstd;C:\Program Files\Mercurial;C:\hostedtoolcache\windows\stack\3.3.1\x64;C:\cabal\bin;C:\ghcup\bin;C:\mingw64\bin;C:\Program Files\dotnet;C:\Program Files\MySQL\MySQL Server 8.0\bin;C:\Program Files\R\R-4.4.2\bin\x64;C:\SeleniumWebDrivers\GeckoDriver;C:\SeleniumWebDrivers\EdgeDriver;C:\SeleniumWebDrivers\ChromeDriver;C:\Program Files (x86)\sbt\bin;C:\Program Files (x86)\GitHub CLI;C:\Program Files\Git\bin;C:\Program Files (x86)\pipx_bin;C:\npm\prefix;C:\hostedtoolcache\windows\go\1.21.13\x64\bin;C:\hostedtoolcache\windows\Python\3.9.13\x64\Scripts;C:\hostedtoolcache\w
 $Env:CONDA_PREFIX = "C:\Miniconda\envs\__setup_conda"
 $Env:CONDA_SHLVL = "1"
 $Env:CONDA_DEFAULT_ENV = "__setup_conda"
 $Env:CONDA_PROMPT_MODIFIER = "(__setup_conda) "
 $Env:CONDA_EXE = "C:\Miniconda\Scripts\conda.exe"
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
 $Env:CONDA_PYTHON_EXE = "C:\Miniconda\python.exe"
